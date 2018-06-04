2f1ef6c674acecad16ef8a98b6cbb18770202692
jslyne <jslyn.vip@gmail.com>
2018-06-04 14:36:59 -0500

    presentacion sprint

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,80 @@
+//Todos los objetos del DOM terminan de cargarse.
+window.onload = window.cipher;
+//Crear los 2 metodos dentro del Window.cipher
 window.cipher = {
-  
-  
-};
+    // Creando la funcion Encode para cifrar el texto
+    encode : (offset, string) => {
+        //Creamos una nueva variable para el cifrado
+        let cipher = '';
+        offset = offset % 26;
+        //Creamos el bucle for para la iteracion
+        for (let i = 0; i < string.length; i++){
+            // Creamos la variable codeAscii para obtener la posicion del codigo ascii del mensaje ingresado
+            let codeAscii = string.charCodeAt(i);
+            let formulaAscii;
+            //Condicion Codigo Ascii - Letra Mayuscula: Se pone la condicion mayor que 65 y menor que 90,
+            //Porque en esa posicion se encuentra las letras mayusculas del ABC en ascii.
+            if (codeAscii >= 65 && codeAscii <=90){
+                //1) Creo la variable para la letra mayuscula = mayusAscii
+                formulaAscii = (codeAscii - 65 + offset) % 26 + 65;
+            }
+            //Condicion Codigo Ascii - Letra Minuscula: Se pone la condicion mayor que 97 y menor que 122,
+            //Porque en esa posicion se encuentra las letras minusculas del ABC en ascii.
+            else if (codeAscii >=97 && codeAscii <=122) {
+                //1) Creo la variable para la letra minuscula = minusAscii
+                formulaAscii = (codeAscii - 97 + offset) % 26 + 97;
+            }
+            //Condicion para el espacio en blanco: Se iguala a  32
+            //Porque en esa posicion se encuentra el espacio en ascii
+            else {
+                formulaAscii = codeAscii;
+            }
+            cipher += String.fromCharCode(formulaAscii);
+        }
+        //Retorna el texto cifrado de la funcion Encode
+        return cipher;
+    },
 
+    //Creando la funcion Decode para descifrar el texto
+    decode : (offset, string) =>{
+        //Creamos una nueva variable para el descifrado
+        let descipher = '';
+        offset = offset % 26;
+        //Creamos el bucle for para la iteracion
+        for (let i=0; i<string.length; i++){
+            // Creamos la variable codeAscii para obtener el codigo ascii del mensaje ingresado
+            let codeAscii = string.charCodeAt(i);
+            let formulaAscii;
+            if (codeAscii >= 65 && codeAscii <= 90){
+                //1) Creo la variable para la letra mayuscula = mayusAscii
+                formulaAscii = (codeAscii - 65 - offset + 26) % 26 + 65;
+            }
+            //Condicion Codigo Ascii - Letra Minuscula: Se pone la condicion mayor que 97 y menor que 122
+            //Porque en esa posicion se encuentra las letras minusculas del ABC en ascii.
+            else if (codeAscii >=97 && codeAscii <=122){
+                //1) Creo la variable para la letra minuscula = minusAscii
+                formulaAscii = (codeAscii - 97 - offset + 26) % 26 + 97;
+            }
+            else {
+                formulaAscii = codeAscii;
+            }
+            descipher += String.fromCharCode(formulaAscii);
+        }
+        //Retorna el texto cifrado de la funcion Encode
+        return descipher;
+    },
+
+    //Creando la funcion CipherWithOfsset
+    createCipherWithOffset : (offset) => {
+        return {
+            encode : (string) => {
+                //Llamando a encode
+                return cipher.encode(offset, string);
+            },
+            decode : (string) => {
+                //Llamando a decode
+                return cipher.decode(offset, string);
+            }
+        };
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,52 +1,78 @@
-let btnEncode = document.getElementById('encode');
-let btnDecode = document.getElementById('decode');
-let mensaje = document.getElementById('mensaje');
-let numero = document.getElementById('key');
-//let result = document.getElementById('resultado');
+//Boton para desplegar cipherArea y ocultar cipherInformation
+    document.getElementById('buttonPrincipal').addEventListener('click', () => {
+    //Desplegando cipherArea
+    document.getElementById('areaCipherMe').style.display = '';
+    //Ocultando cipherInformation
+    document.getElementById('infoCipherMe').style.display = 'none';
+});
+//Boton para volver al incio de la pagina
+    document.getElementById('linkLogo').addEventListener('click', () => {
+    //Ocultando cipherArea
+    document.getElementById('areaCipherMe').style.display = 'none';
+    //Ocultando cipherSteps
+    document.getElementById('stepCipherMe').style.display = 'none';
+    //Desplegando cipherInformation
+    document.getElementById('infoCipherMe').style.display = '';
+});
+//1.Evento: BOTON CIFRAR
+document.getElementById('cipher').addEventListener('click', () => {
+    const string = document.getElementById('inputMensaje').value;
+    const offset = parseInt(document.getElementById('numeroKey').value)%26;
+    const outputCifrado = document.getElementById('outputCifrado');
+    outputCifrado.innerHTML = cipher.encode(offset, string);
+});
 
-btnEncode.addEventListener('click', encode);
-function encode() {
-    let txtResultado = '';
-    let txtMensaje = mensaje.value;
-    let key = parseInt(numero.value);
-    for (i=0; i < txtMensaje.length; i++){
-        let toAscii = txtMensaje.charCodeAt(i);
-            if (toAscii>=65 && toAscii<=90){
-                txtResultado += String.fromCharCode((toAscii - 65 + key) % 26 + 65);
-            }
-            else if (toAscii >= 97 && toAscii <= 122){
-                txtResultado += String.fromCharCode((toAscii - 97 + key) % 26 + 97);
-            }
-            else
-            //result.innerHTML= `<p> ${txtResultado} </p>`;
-            document.getElementById('solucion').value = ""+txtResultado+"";
-    }
-    //result.innerHTML= `<p> ${txtResultado} </p>`;
-    document.getElementById('solucion').value = ""+txtResultado+"";
-}
+//2.Evento: BOTON DESCIFRAR
+document.getElementById('descipher').addEventListener('click',() => {
+    const string = document.getElementById('inputMensaje').value;
+    const offset = parseInt(document.getElementById('numeroKey').value)%26;
+    const outputDescifrado = document.getElementById('outputDescifrado');
+    outputDescifrado.innerHTML = cipher.decode(offset, string);
+});
 
-btnDecode.addEventListener('click', decode);
-function decode() {
-    let txtResultado = '';
-    let txtMensaje = mensaje.value;
-    let key = parseInt(numero.value);
-    for (i=0; i < txtMensaje.length; i++){
-        let toAscii = txtMensaje.charCodeAt(i);
-            if (toAscii>=65 && toAscii<=90){
-                txtResultado += String.fromCharCode((toAscii - 65 - key + 26 * 2) % 26 + 65);
-            }
-            else if (toAscii >= 97 && toAscii <= 122){
-                txtResultado += String.fromCharCode((toAscii - 97 - key + 26 * 2) % 26 + 97);
-            }
-            else
-            //result.innerHTML= `<p> ${txtResultado} </p>`;
-            document.getElementById('solucion').value = ""+txtResultado+"";
-    }
-    //result.innerHTML= `<p> ${txtResultado} </p>`;
-    document.getElementById('solucion').value = ""+txtResultado+"";
-}
+//COPIAR TEXTO CIFRADO
+document.getElementById('copyCipher').addEventListener('click', () => {
+    // Crea un campo de texto "oculto"
+    var aux = document.createElement('input');
+    // Asigna el contenido del elemento especificado al valor del campo
+    aux.setAttribute('value', document.getElementById('outputCifrado').innerHTML);
+    // Añade el campo a la página
+    document.body.appendChild(aux);
+    // Selecciona el contenido del campo
+    aux.select();
+    // Copia el texto seleccionado
+    document.execCommand('copy');
+    // Elimina el campo de la página
+    document.body.removeChild(aux);
+});
 
-function clear (){
-    document.getElementById("form").reset();
-}
+//COPIAR TEXTO DESCIFRADO
+document.getElementById('copyDescipher').addEventListener('click', () =>{
+    // Crea un campo de texto "oculto"
+    var aux = document.createElement('input');
+    // Asigna el contenido del elemento especificado al valor del campo
+    aux.setAttribute('value', document.getElementById('outputDescifrado').innerHTML);
+    // Añade el campo a la página
+    document.body.appendChild(aux);
+    // Selecciona el contenido del campo
+    aux.select();
+    // Copia el texto seleccionado
+    document.execCommand('copy');
+    // Elimina el campo de la página
+    document.body.removeChild(aux);
+});
 
+//LIMPIAR INPUT MENSAJE
+document.getElementById('clean').addEventListener('click', () =>{
+    document.getElementById('inputMensaje').value = "";
+});
+
+//RESETEAR TODO
+document.getElementById('refresh').addEventListener('click', () =>{
+    window.location.reload();
+});
+
+//SALIR
+document.getElementById('salir').addEventListener('click', () =>{
+    window.close();
+});
